e2fe29638520b17e78383fc7fa8e896890335571
初コミット <yodatakahiro@MacBook-Pro-3.local>
2016-10-07 17:56:06 +0900

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-User.new(email: 'takahirono4444@gmail.com', password: 'taka3093', nickname: 'takahiroyoda')
+User.create(email: 'yuko@limetrd.co.jp', password: 'yuko1978', nickname: 'yukoyoda')
+
+User.create(email: 'takahirono4444@gmail.com', password: 'taka3093', nickname: 'takahiroyoda')
