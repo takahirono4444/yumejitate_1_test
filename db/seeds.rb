7dd9233f9fab4d0b24b805ebd278b269f2ae8715
初コミット <yodatakahiro@MacBook-Pro-3.local>
2016-10-10 17:24:21 +0900

    commit all

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+User.create(email: 'yamamoto', password: 'yamamoto', nickname: 'yamamoto')
 User.create(email: 'ichinenichikumi@limetrd.co.jp', password: 'lime1946', nickname: 'mitsuhiroyoda')
 User.create(email: 'yuko@limetrd.co.jp', password: 'yuko1978', nickname: 'yukoyoda')
 
