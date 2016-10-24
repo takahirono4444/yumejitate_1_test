3a43a472d2e46bf8db802e690f5ac64a0983e403
初コミット <yodatakahiro@MacBook-Pro-3.local>
2016-10-24 21:41:41 +0900

    delete each pic from _list

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,26 @@ Rails.application.routes.draw do
   devise_for :users
   root "products#index"
 
-  resources :finishs
+  resources :finishs do
+    collection do
+    get 'search1'
+      get 'search'
+      get 'find'
+      get 'find2'
+      get 'find3'
+      get 'find4'
+      get 'find5'
+      get 'find6'
+      get 'find7'
+      get 'find8'
+end
+end
 
-
+resources :pictures
 
  resources :products do
   
-    resources :pictures
+    
   collection do
     get 'search1'
       get 'search'
